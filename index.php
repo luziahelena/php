20ca4dce25aa39019d998f576aa9a3d29ca8013f
Técnico em Informática - 2018.3 <169121742018.3@senacrjedu.local>
2019-10-31 10:43:29 -0300

    explicando

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+include_once'topo.php';
+?>
+    <a href="cadastrar.php">
+        CADASTRAR 
+        </a>
+
+        <a href="login.php">
+        Logar no sistema 
+        </a>
+    </div>
+</body>
+</html>
